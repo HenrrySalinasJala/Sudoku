82e013ddbe19864f350a77e9efd0fc8d5109ba4d
Miguel Vega Quiroz <MiguelVega@Fundacion-jala.local>
2016-03-21 14:12:58 -0400

    Add csvhandler and test of this

---
+++ sudokuProject/test/CSVHandlerTest.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import sudoku.io.CSVHandler;
+
+public class CSVHandlerTest  {
+	
+	@Test
+	public void CSVHandlerCanBeCreatedWithNoParameters() {
+		CSVHandler testCSVHandler = new CSVHandler();
+		assertNotNull(testCSVHandler);
+	}
+	
+	@Test
+	public void CSVHandlerCanWriteandReadCSVs()
+	{
+		CSVHandler testCSVHandler = new CSVHandler();
+		testCSVHandler.csvWrite("0", "0", "0", "0", "0", "0", "0", "0", "0");
+		testCSVHandler.csvRead();		
+		assertEquals(testCSVHandler.getspot1(),"0");
+	}
+}
+
