0a98123d5e7cc5c33aaac393182ddc605f7b7b5b
Henrry Salinas <es.henrry@gmail.com>
2016-03-26 20:50:07 -0400

    Applying pattern designs

--- sudokuProject/src/Main.java
+++ sudokuProject/src/Main.java
@@ -1,15 +1,13 @@
 import sudoku.GameBoard;
 
 public class Main {
-
-	public static void main(String[] args) {
-		GameBoard sudo=new GameBoard();
-		System.out.println("BEFORE\n");
-		sudo.printGrid(sudo.grid);
-		
-		sudo.solveGrid();
-		System.out.println("AFTER\n");
-		sudo.printGrid(sudo.grid);
-	}
-
+    public static void main(String[] args)
+    {
+        GameBoard sudo = new GameBoard();
+        System.out.println("BEFORE\n");
+        sudo.printGrid(sudo.grid);
+        sudo.solveGrid();
+        System.out.println("AFTER\n");
+        sudo.printGrid(sudo.grid);
+    }
 }
--- sudokuProject/test/XMLHandlerTest.java
+++ sudokuProject/test/XMLHandlerTest.java
@@ -1,27 +1,25 @@
 import static org.junit.Assert.*;
-
 import org.junit.Test;
-
 import sudoku.Settings;
 import sudoku.io.XMLHandler;
 
 public class XMLHandlerTest {
-	@Test
-	public void aXMLFileCanBeCreateWithLevelOutputAndAlgorithm() throws Exception
-	{
-		Settings set = new Settings("levelXMLHandlerTest", "algorithmXMLHandlerTest", ".\\files\\xmlHandlerTest.xml");
-		XMLHandler xml = new XMLHandler(set.getLevel(), set.getAlgorithm(), set.getOuput());
-		xml.writeXML();
-	}
-	@Test
-	public void theLevelOutputAndAlgorithmInformationCanBeReaded() throws Exception
-	{
-		Settings set = new Settings("levelXMLHandlerTest", "algorithmXMLHandlerTest", "xmlHandlerTest.xml");
-		XMLHandler xml = new XMLHandler(set.getLevel(), set.getAlgorithm(), set.getOuput());
-		xml.writeXML();
-		xml.readXML(".\\files\\xmlHandlerTest.xml");
-		assertEquals(xml.getLevel(), "levelXMLHandlerTest");
-		assertEquals(xml.getAlgorithm(), "algorithmXMLHandlerTest");
-		assertEquals(xml.getOutput(), ".\\files\\xmlHandlerTest.xml");
-	}
+    @Test
+    public void aXMLFileCanBeCreateWithLevelOutputAndAlgorithm() throws Exception
+    {
+        Settings set = new Settings("levelXMLHandlerTest", "algorithmXMLHandlerTest", ".\\files\\xmlHandlerTest.xml");
+        XMLHandler xml = new XMLHandler(set.getLevel(), set.getAlgorithm(), set.getOuput());
+        xml.writeXML();
+    }
+    @Test
+    public void theLevelOutputAndAlgorithmInformationCanBeReaded() throws Exception
+    {
+        Settings set = new Settings("levelXMLHandlerTest", "algorithmXMLHandlerTest", ".\\files\\xmlHandlerTest.xml");
+        XMLHandler xml = new XMLHandler(set.getLevel(), set.getAlgorithm(), set.getOuput());
+        xml.writeXML();
+        xml.readXML(".\\files\\xmlHandlerTest.xml");
+        assertEquals(xml.getLevel(), "levelXMLHandlerTest");
+        assertEquals(xml.getAlgorithm(), "algorithmXMLHandlerTest");
+        assertEquals(xml.getOutput(), ".\\files\\xmlHandlerTest.xml");
+    }
 }
